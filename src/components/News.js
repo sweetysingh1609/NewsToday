965a27950fcd8a00f3d2590f8326905529c94c13
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-01-08 13:30:10 +0530

    App structure

---
+++ src/components/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is news
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>This a new NewsItem</div>
+    )
+  }
+}
+
+export default NewsItem
