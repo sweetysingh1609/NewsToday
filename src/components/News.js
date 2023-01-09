7c044c1aac8de96c78772253305bf47af1114e8c
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-01-09 17:23:21 +0530

    Created news cards

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news
-      <NewsItem/>
-      <NewsItem/>
-      <NewsItem/>
-      <NewsItem/>
-      <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" />
+          </div>
+        
+        </div>
+
+      
+        
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ModalTitle } from "react-bootstrap";
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props
     return (
-      <div>This a new NewsItem</div>
-    )
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://www.reuters.com/resizer/ZhHs8eRERAgPmDbHzQuy0lcTpNM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/2OQPDBRJVVJJXBTXQO3F76R6SE.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title"> {title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
