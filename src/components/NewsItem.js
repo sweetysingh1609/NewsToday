4e0ea36384d5b1e62fe457f91fdcb9d078b4b2d1
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-01-10 12:58:16 +0530

    Displayed data in card

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,21 +2,18 @@ import React, { Component } from "react";
 import { ModalTitle } from "react-bootstrap";
 
 export class NewsItem extends Component {
+  
   render() {
-    let {title, description} = this.props
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://www.reuters.com/resizer/ZhHs8eRERAgPmDbHzQuy0lcTpNM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/2OQPDBRJVVJJXBTXQO3F76R6SE.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title"> {title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title"> {title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">
+              Read More
             </a>
           </div>
         </div>
