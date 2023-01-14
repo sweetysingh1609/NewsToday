8fab591e002b75b37f5e98d6d4855509e44cacff
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-01-14 14:57:26 +0530

    Implemented different news handles on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-
+import {  Link } from "react-router-dom";
 export class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <Link className="navbar-brand" to="#">
               NewsToday
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -27,49 +27,49 @@ export class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link " aria-current="page" href="/">
+                  <Link className="nav-link " aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">
+                {/* <li className="nav-item">
+                  <Link className="nav-link" to="/about">
                     About
-                  </a>
+                  </Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    business
-                  </a>
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    entertainment
-                  </a>
+                  <Link className="nav-link" to="/">
+                    General
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    general
-                  </a>
+                  <Link className="nav-link" to="/health">
+                    Health
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    health
-                  </a>
+                  <Link className="nav-link" to="/science">
+                    Science
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    science
-                  </a>
+                  <Link className="nav-link" to="/sports">
+                    Sports
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    sports
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    technology
-                  </a>
+                  <Link className="nav-link" to="/technology">
+                    Technology
+                  </Link>
                 </li>
               </ul>
             </div>
